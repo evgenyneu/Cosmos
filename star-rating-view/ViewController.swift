8606802412886a74e779061b9919dfcd88e67879
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-28 13:54:42 +1100

    Initial Commit

---
+++ star-rating-view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  star-rating-view
+//
+//  Created by Evgenii Neumerzhitckii on 28/11/2014.
+//  Copyright (c) 2014 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
