564ebbc7b59fc094c8b3bac2fa8b74ea81137562
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-09-21 11:58:35 +1000

    Add performance row

--- Demo/PerformanceTableViewController.swift
+++ Demo/PerformanceTableViewController.swift
@@ -4,4 +4,21 @@ class PerformanceTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  // MARK: - UITableViewDataSource
+  
+  override func tableView(tableView: UITableView,
+    cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      
+    if let cell = tableView.dequeueReusableCellWithIdentifier("performanceTableViewCell") as? PerformanceTableViewCell  {
+      cell.update(2.3)
+      return cell
+    }
+      
+    return UITableViewCell()
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
 }
