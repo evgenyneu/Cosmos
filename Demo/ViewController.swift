a2c801d81daf6013de81f58700632bd80e9e3975
Max <maxzheleznyy@gmail.com>
2020-03-01 15:54:18 -0800

    Fix rating update responsiveness in Demo

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var ratingSlider: UISlider!
   @IBOutlet weak var ratingLabel: UILabel!
-  private let startRating:Float = 3.7
+  private let startRating: Float = 3.7
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,21 +25,29 @@ class ViewController: UIViewController {
     cosmosViewHalf.didFinishTouchingCosmos = didFinishTouchingCosmos
     cosmosViewPrecise.didFinishTouchingCosmos = didFinishTouchingCosmos
     
+    //update with initial data
     ratingSlider.value = startRating
-    updateRating()
+    updateRating(requiredRating: nil)
   }
   
   @IBAction func onSliderChanged(_ sender: AnyObject) {
-    updateRating()
+    updateRating(requiredRating: nil)
   }
   
-  private func updateRating() {
-    let value = Double(ratingSlider.value)
-    cosmosViewFull.rating = value
-    cosmosViewHalf.rating = value
-    cosmosViewPrecise.rating = value
+    private func updateRating(requiredRating: Double?) {
+    var newRatingValue: Double = 0
+        
+    if let nonEmptyRequiredRating = requiredRating {
+        newRatingValue = nonEmptyRequiredRating
+    } else {
+        newRatingValue = Double(ratingSlider.value)
+    }
+        
+    cosmosViewFull.rating = newRatingValue
+    cosmosViewHalf.rating = newRatingValue
+    cosmosViewPrecise.rating = newRatingValue
     
-    self.ratingLabel.text = ViewController.formatValue(value)
+    self.ratingLabel.text = ViewController.formatValue(newRatingValue)
   }
   
   private class func formatValue(_ value: Double) -> String {
@@ -48,6 +56,7 @@ class ViewController: UIViewController {
   
   private func didTouchCosmos(_ rating: Double) {
     ratingSlider.value = Float(rating)
+    updateRating(requiredRating: rating)
     ratingLabel.text = ViewController.formatValue(rating)
     ratingLabel.textColor = UIColor(red: 133/255, green: 116/255, blue: 154/255, alpha: 1)
   }
